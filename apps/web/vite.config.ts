b7ee7c354d26a2b2826957b6c4eec98fe8ded299
prem-1214 <rajputpremkumar22@gmail.com>
2026-06-27 19:36:22 +0530

    refactor(vite-config): refactor vite config to use chunking

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  base: '/',
-})
+  plugins: [tailwindcss(), react()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router")
+            )
+              return "react-vendor";
+            if (id.includes("framer-motion")) return "framer";
+            if (id.includes("docx-preview")) return "docx";
+            if (id.includes("react-markdown") || id.includes("remark"))
+              return "markdown";
+            if (id.includes("lucide")) return "icons";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
